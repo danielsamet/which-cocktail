351d6f275fe2485c123ebf1b69c569361e4c9d3d
Daniel <danielsamet98@gmail.com>
2022-09-22 21:40:21 +0100

    feat: update cocktail lists

---
+++ src/cocktails.ts
@@ -0,0 +1,355 @@
+export type Cocktail = {
+  name: string;
+  ingredients: string[];
+};
+
+export const cocktails: Cocktail[] = [
+  {
+    name: "Martini",
+    ingredients: ["dry gin", "dry vermouth"],
+  },
+  {
+    name: "Old Fashioned",
+    ingredients: ["sugar cube", "angostura bitters", "soda water", "bourbon"],
+  },
+  {
+    name: "Cosmopolitan",
+    ingredients: ["vodka", "triple sec", "cranberry juice"],
+  },
+  {
+    name: "Long Island Iced Tea",
+    ingredients: [
+      "vodka",
+      "gin",
+      "light rum",
+      "tequila",
+      "lemon juice",
+      "orange liqueur",
+      "sugar",
+    ],
+  },
+  {
+    name: "Gin Rickey",
+    ingredients: ["gin", "lime juice", "simple syrup", "soda water"],
+  },
+  {
+    name: "Whiskey Sour",
+    ingredients: [
+      "bourbon",
+      "cherry juice",
+      "angostura bitters",
+      "lemon juice",
+      "simple syrup",
+    ],
+  },
+  {
+    name: "Rob Roy",
+    ingredients: [
+      "vodka",
+      "scotch whiskey",
+      "sweet vermouth",
+      "angostura bitters",
+    ],
+  },
+  {
+    name: "Americano",
+    ingredients: ["campari", "sweet vermouth", "soda water"],
+  },
+  {
+    name: "Singapore Sling",
+    ingredients: [
+      "gin",
+      "cherry brandy",
+      "lemon juice",
+      "grenadine",
+      "soda water",
+    ],
+  },
+  { name: "Negroni", ingredients: ["dry gin", "sweet vermouth", "campari"] },
+  {
+    name: "Manhattan",
+    ingredients: ["bourbon", "sweet vermouth", "angostura bitters"],
+  },
+  { name: "Gimlet", ingredients: ["gin", "lime juice"] },
+  {
+    name: "Champagne Cocktail",
+    ingredients: ["sugar cube", "angostura bitters", "brandy", "champagne"],
+  },
+  { name: "Moscow Mule", ingredients: ["lime", "vodka", "ginger beer"] },
+  {
+    name: "Brooklyn Dropout",
+    ingredients: [
+      "champagne",
+      "ginger beer",
+      "lemon juice",
+      "elderflower liqueur",
+      "angostura bitters",
+      "lime",
+      "peach",
+    ],
+  },
+  {
+    name: "Watermelon Rum Punch",
+    ingredients: [
+      "watermelon",
+      "white rum",
+      "tequila",
+      "mint syrup",
+      "lime juice",
+      "lemonade",
+    ],
+  },
+  {
+    name: "St Petersburger Punch",
+    ingredients: [
+      "russian vodka",
+      "lemon juice",
+      "creme de cassis",
+      "fresh raspberry puree",
+      "raspberry liqueur",
+      "simple syrup",
+    ],
+  },
+  {
+    name: "Almost Classic Sangria",
+    ingredients: ["red wine", "lemonade", "orange juice"],
+  },
+  {
+    name: "Honey Beer Punch",
+    ingredients: ["honey", "lemon juice", "gin", "beer"],
+  },
+  {
+    name: "Beer Team",
+    ingredients: ["beer", "ginger beer", "english breakfast tea"],
+  },
+  {
+    name: "Planter's Punch",
+    ingredients: [
+      "dark rum",
+      "lime juice",
+      "angostura bitters",
+      "simple syrup",
+      "soda water",
+    ],
+  },
+  {
+    name: "Poor Man's Champagne Punch",
+    ingredients: [
+      "frozen raspberries",
+      "lime juice",
+      "simple syrup",
+      "vodka",
+      "beer",
+    ],
+  },
+  {
+    name: "Rosita",
+    ingredients: [
+      "tequila",
+      "sweet vermouth",
+      "dry vermouth",
+      "aperol",
+      "angostura bitters",
+    ],
+  },
+  {
+    name: "Apple Catcher",
+    ingredients: ["apple brandy", "orange juice", "lemon juice", "maple syrup"],
+  },
+  {
+    name: "Spartacus",
+    ingredients: [
+      "scotch whiskey",
+      "orange juice",
+      "sweet vermouth",
+      "cherry heering",
+    ],
+  },
+  { name: "Take Ivy", ingredients: ["gin", "orange bitters", "port"] },
+  {
+    name: "Espresso Martini",
+    ingredients: [
+      "vodka",
+      "cold espresso",
+      "coffee liqueur",
+      "brown sugar syrup",
+    ],
+  },
+  {
+    name: "Holy Water",
+    ingredients: [
+      "vodka",
+      "triple sec",
+      "white rum",
+      "tonic water",
+      "grenadine",
+    ],
+  },
+  {
+    name: "Maxim",
+    ingredients: ["gin", "dry vermouth", "dark creme de cacao"],
+  },
+  {
+    name: "The Slammer",
+    ingredients: ["tequila gold", "champagne", "creme de cacao"],
+  },
+  {
+    name: "Sour Fizz",
+    ingredients: [
+      "tequila gold",
+      "lime juice",
+      "runny honey",
+      "orange bitters",
+      "lemon",
+    ],
+  },
+  {
+    name: "Dirty Dick",
+    ingredients: [
+      "tequila gold",
+      "black pepper vodka",
+      "lime juice",
+      "caper berry brine",
+    ],
+  },
+  {
+    name: "Class Act",
+    ingredients: [
+      "sugar cube",
+      "angostura bitters",
+      "grand marnier",
+      "champagne",
+    ],
+  },
+  { name: "Rudolph", ingredients: ["gin", "elderflower liqueur", "champagne"] },
+  {
+    name: "Pina Colada",
+    ingredients: ["white rum", "dark rum", "pineapple juice", "coconut cream"],
+  },
+  {
+    name: "Mojito",
+    ingredients: ["lime juice", "sugar", "mint", "light rum", "soda water"],
+  },
+  {
+    name: "Peach & Black Pepper Margarita",
+    ingredients: [
+      "black pepper",
+      "lime juice",
+      "ripe peach",
+      "black pepper infused tequila",
+      "peach schnapps",
+    ],
+  },
+  {
+    name: "Tequila Pomegranate",
+    ingredients: ["tequila", "cointreau", "lime juice", "pomegranate juice"],
+  },
+  {
+    name: "Banana Daiquiri",
+    ingredients: [
+      "dark rum",
+      "banana liqueur",
+      "lime juice",
+      "simple syrup",
+      "banana",
+    ],
+  },
+  { name: "Beach House", ingredients: ["gin", "lime juice", "coconut water"] },
+  {
+    name: "Big Cardomomma's House",
+    ingredients: [
+      "mango",
+      "peach schnapps",
+      "lime juice",
+      "cardamom syrup",
+      "champagne",
+    ],
+  },
+  {
+    name: "Pineapple Mountain",
+    ingredients: [
+      "aged rum",
+      "pineapple juice",
+      "lemon juice",
+      "ginger syrup",
+      "angostura bitters",
+    ],
+  },
+  {
+    name: "Frozen Ginger Margarita",
+    ingredients: [
+      "tequila",
+      "cointreau",
+      "ginger syrup",
+      "lime",
+      "lemon juice",
+      "orange juice",
+      "champagne",
+    ],
+  },
+  {
+    name: "Watermelon Bloody Mary",
+    ingredients: [
+      "vodka",
+      "dry white wine",
+      "watermelon juice",
+      "lemon juice",
+      "mint leaves",
+      "ground pepper",
+      "angostura bitters",
+    ],
+  },
+  {
+    name: "Bloody Mary",
+    ingredients: [
+      "vodka",
+      "dry white wine",
+      "tabasco",
+      "worcestershire sauce",
+      "tomato juice",
+      "lemon juice",
+      "celery salt",
+      "cayenne pepper",
+    ],
+  },
+  {
+    name: "Fire in the Blood",
+    ingredients: [
+      "dill",
+      "dijon mustard",
+      "tabasco",
+      "worcestershire sauce",
+      "black pepper infused vodka",
+      "tomato juice",
+      "lemon juice",
+      "celery salt",
+      "ground pepper",
+    ],
+  },
+  {
+    name: "Sangrita",
+    ingredients: [
+      "tequila",
+      "dry sherry",
+      "creamed horseradish",
+      "worcestershire sauce",
+      "clamato juice",
+      "lemon juice",
+      "celery salt",
+      "cayenne pepper",
+    ],
+  },
+  {
+    name: "Wasabian",
+    ingredients: [
+      "wasabi",
+      "celery salt",
+      "lime juice",
+      "lemon juice",
+      "vodka",
+      "sake",
+      "tomato juice",
+      "angostura bitters",
+    ],
+  },
+];
