ec62d38b0408726b66707f14813a9d4df856c00e
Daniel <danielsamet98@gmail.com>
2023-03-28 21:00:58 +0100

    feat: add CheckboxGroup count

--- src/Components/CheckboxGroup.tsx
+++ src/Components/CheckboxGroup.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Heading } from "@chakra-ui/react";
+import { Badge, Box, Divider, Flex, Heading, Spacer } from "@chakra-ui/react";
 import React, { Dispatch, SetStateAction } from "react";
 import { Checkbox, CheckboxItem } from "./Checkbox";
 
@@ -10,6 +10,14 @@ type CheckboxGroupProps<Item extends CheckboxItem<Item>> = {
   checkWithProperties?: (keyof Item)[];
 };
 
+/**
+ * CheckboxGroup
+ * @param title
+ * @param items
+ * @param selectedItems [these are items that have been (and should be marked as) selected - can include more items than the items parameter]
+ * @param setSelectedItems
+ * @param checkWithProperties
+ */
 export const CheckboxGroup = <Item extends CheckboxItem<Item>>({
   title,
   items,
@@ -17,11 +25,28 @@ export const CheckboxGroup = <Item extends CheckboxItem<Item>>({
   setSelectedItems,
   checkWithProperties,
 }: CheckboxGroupProps<Item>) => {
+  const selectedItemCount = selectedItems.filter((selectedItem) =>
+    items.some((item) =>
+      checkWithProperties === undefined
+        ? item === selectedItem
+        : checkWithProperties.every(
+            (property) => item[property] === selectedItem[property]
+          )
+    )
+  ).length;
+  const itemCount = items.length;
+
   return (
     <Box p={5}>
-      <Heading size={"md"} textTransform={"capitalize"}>
-        {title}
-      </Heading>
+      <Flex>
+        <Heading size={"md"} textTransform={"capitalize"}>
+          {title}
+        </Heading>
+        <Spacer />
+        <Badge colorScheme="green" px={2} py={1} mr={2} mb={1}>
+          {selectedItemCount}/{itemCount}
+        </Badge>
+      </Flex>
       <Divider borderWidth={"1px"} borderColor={"#ffffffeb"} mb={2} />
 
       {items.map((item, index) => {
