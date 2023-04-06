33b61700f326a1b39d89721961012e49afadac8d
Daniel <danielsamet98@gmail.com>
2023-04-06 19:58:38 +0100

    feat(types): add component topping or qty&measure

--- src/data/types.ts
+++ src/data/types.ts
@@ -19,16 +19,34 @@ export type Ingredient<
   aliases?: string[];
   address?: string;
   urlSafeName?: string;
-};
+} & RoutableItem;
 
 export type Cocktail = {
   name: string;
-  components: {
-    qty?: number;
-    measure?: "measure" | "ml" | "tsp" | "tbsp";
-    ingredient: Ingredient;
-  }[];
+  components: CocktailComponent[];
+  garnishes?: Ingredient<"fruit" | "other">[];
   description?: string;
+} & RoutableItem;
+
+type RoutableItem = {
   address?: string;
   urlSafeName?: string;
 };
+
+type CocktailComponent = NAND<
+  {
+    qty: number;
+    measure: "measure" | "ml" | "tsp" | "tbsp" | "cube" | "dash" | "piece";
+  },
+  {
+    top: boolean;
+  }
+> & {
+  ingredient: Ingredient;
+};
+
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
+type XOR<T, U> = T | U extends object
+  ? (Without<T, U> & U) | (Without<U, T> & T)
+  : T | U;
+type NAND<T, U> = Without<T, U> | XOR<T, U>;
